723d9320659fc2917f4429e44d649d48f4dc1e50
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-01-21 21:41:34 -0300

    Initial Commit feat: application.properties and application-test.properties config

---
+++ src/main/java/com/generic/MartialManager/MartialManagerApplication.java
@@ -0,0 +1,13 @@
+package com.generic.MartialManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartialManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartialManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generic/MartialManager/MartialManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generic.MartialManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartialManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
